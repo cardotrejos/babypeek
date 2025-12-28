46b7391a39689a1ca3a11e4041c04ae015bb3c31
Ricardo Trejos <cardotrejos@gmail.com>
2025-12-28 15:17:34 -0500

    fix: posthog

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
+import { PostHogProvider } from "./lib/posthog";
 import "./index.css";
 
 const router = getRouter();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <PostHogProvider>
+      <RouterProvider router={router} />
+    </PostHogProvider>
   </StrictMode>,
 );
